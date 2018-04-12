fc653cfae04141f16aa637d2670ee19b9d1117b5
rshawabkeh <alshawabkeh.rami.1987@gmail.com>
2018-04-12 09:32:00 +0300

    ITG

--- src/test/java/IMAMProject/AThirdPartyUser/SmokeTest.java
+++ src/test/java/IMAMProject/AThirdPartyUser/SmokeTest.java
@@ -4,39 +4,61 @@
 
 package IMAMProject.AThirdPartyUser;
 
-import io.github.bonigarcia.wdm.ChromeDriverManager;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
-import sun.awt.windows.WEmbeddedFrame;
+
+import static IMAMProject.Login.driverImam;
+import static IMAMProject.Login.waitImam;
 
 public class SmokeTest {
 
+    By personalInformationLoactor = By.id("divMenuItem_1");
+
+    @Test
+    public void personalInformation() {
+
+        WebElement personalInformationLoactorWait = waitImam.until(ExpectedConditions.visibilityOfElementLocated(personalInformationLoactor));
+        personalInformationLoactorWait.click();
+    }
+
+    By Training = By.id("divMenuItem_1361");
+    By RegisterLoacotor = By.xpath("/html/body/form/div[7]/div[2]/div[2]/div/div/div/div[2]/div[3]/div[1]/div/div/div[2]/a");
+    By SreachButtonLocator = By.id("ctl00_PlaceHolderMain_ibtnView");
 
 
     @Test
-    public void loginToIMMAMAndSwitchPrfile(){
-
-        ChromeDriverManager.getInstance().setup();
-        WebDriver driverImam=new ChromeDriver();
-        driverImam.navigate().to("https://10.0.1.195/EduWaveKSA_Imam/Login.aspx");
-        By UserNameLOctor= By.id("tbPublic");
-        By PassWordLocator=By.id("tbPrivate");
-
-        WebDriverWait waitImam=new WebDriverWait(driverImam,10);
-
-        WebElement UserNameLOctorWait=waitImam.until(ExpectedConditions.visibilityOfElementLocated(UserNameLOctor));
-        UserNameLOctorWait.sendKeys("2354571560");
-        WebElement PassWordLocatorWait=waitImam.until(ExpectedConditions.visibilityOfElementLocated(PassWordLocator));
-        PassWordLocatorWait.sendKeys("1234",Keys.ENTER);
+    public void training() throws InterruptedException {
 
 
+        WebElement TrainingWait = waitImam.until(ExpectedConditions.visibilityOfElementLocated(Training));
+        TrainingWait.click();
 
+        WebElement RegisterLoacotorWait = waitImam.until(ExpectedConditions.visibilityOfElementLocated(RegisterLoacotor));
+        RegisterLoacotorWait.click();
+
+        WebElement SreachButtonLocatorWait = waitImam.until(ExpectedConditions.visibilityOfElementLocated(SreachButtonLocator));
+        SreachButtonLocatorWait.click();
+    }
+
+    @Test
+    public void queryTheResult() {
+
+        By queryTheResult = By.id("divMenuItem_6897");
+
+        WebElement queryTheResultWait = waitImam.until(ExpectedConditions.visibilityOfElementLocated(queryTheResult));
+        queryTheResultWait.click();
+
+
+    }
+
+    @Test
+    public void downloadData() {
+        By downloadData = By.id("divMenuItem_6907");
+
+        WebElement downloadDataWait = waitImam.until(ExpectedConditions.visibilityOfElementLocated(downloadData));
+        downloadDataWait.click();
 
 
     }
