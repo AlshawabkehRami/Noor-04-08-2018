16249ca6fbfe1be247d41f4582bdd3854a6fd66f
rshawabkeh <alshawabkeh.rami.1987@gmail.com>
2018-04-15 13:52:50 +0300

    ITG

--- src/test/java/NoorProject/Other/NoorLogOut.java
+++ src/test/java/NoorProject/Other/NoorLogOut.java
@@ -1,9 +1,12 @@
 package NoorProject.Other;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.Test;
 
 import static NoorProject.Other.NoorLogin.browserQA;
+import static NoorProject.Other.NoorLogin.waitQA;
 
 public class NoorLogOut {
 
@@ -15,7 +18,8 @@ public class NoorLogOut {
 
         Thread.sleep(1000);
 
-        browserQA.findElement(LogOutLocator).click();
+        WebElement LogOutLocatorWait=waitQA.until(ExpectedConditions.visibilityOfElementLocated(LogOutLocator));
+        LogOutLocatorWait.click();
 
         browserQA.close();
 
